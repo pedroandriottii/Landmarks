c1cbb4d7a602fed3658319d006d4081f7d2242b4
Pedro Henrique Andriotti Bastos <phab@cesar.school>
2024-09-02 09:58:47 -0300

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Pedro Andriotti on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
