7478519c1fc4682cbe611ef8cb319d8f4dff7ce3
Pedro Henrique Andriotti Bastos <phab@cesar.school>
2024-09-02 13:07:47 -0300

    Building lists and navigations

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Pedro Andriotti on 02/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,38 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Pedro Andriotti on 02/09/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack{
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+        
+    }
+}
+
+#Preview{
+    Group{
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
+
+#Preview("Turtle Rock"){
+    LandmarkRow(landmark: landmarks[0])
+}
+#Preview("Salmon"){
+    LandmarkRow(landmark: landmarks[1])
+}
